704192d3b455ff46179cc090d68a230020772edc
Tiago Mendes <tetigo@gmail.com>
2023-06-28 15:42:45 -0300

    feat: carousel

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <NavBar></NavBar>
+  <NavBar />
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <CarouselItem />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-6 d-flex justify-content-center">
+        text1
+      </div>
+      <div class="col-md-8 col-6 d-flex justify-content-center">
+        text2
+      </div>
+      <div class="col-md-2 col-12 d-flex justify-content-center">
+        text3
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import CarouselItem from './components/CarouselItem.vue'
 import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar,
+    CarouselItem,
   },
 }
 </script>
---
+++ src/components/CarouselItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    id="carouselExampleControls"
+    class="carousel slide carousel-custom"
+    data-bs-ride="carousel"
+  >
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img
+          src="@/assets/botties.jpg"
+          class="d-block w-100"
+          alt="little booties"
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="@/assets/fashion.jpg"
+          class="d-block w-100"
+          alt="fashion blouse"
+        />
+      </div>
+      <div class="carousel-item">
+        <img src="@/assets/scarpin.jpg" class="d-block w-100" alt="scarpin" />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarouselItem',
+}
+</script>
+
+<style>
+.carousel-custom {
+  max-width: 600px;
+}
+</style>
